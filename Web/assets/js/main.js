52b8c1e3f7a3666008a0165c1da975b55964ce55
yrc <haoran937@gmail.com>
2020-07-08 22:37:57 +0800

    修改弹出层逻辑，将公共元素模块化

--- Web/assets/js/main.js
+++ Web/assets/js/main.js
@@ -1,12 +1,39 @@
-//获取cookie
-function getCookie(cookieName) {
-    var strCookie = document.cookie;
-    var arrCookie = strCookie.split("; ");
-    for(var i = 0; i < arrCookie.length; i++){
-        var arr = arrCookie[i].split("=");
-        if(cookieName == arr[0]){
-            return arr[1];
-        }
-    }
-    return "";
+
+//显示登录窗口
+function displayLoginWindow(){
+	layui.use('layer', function(){
+		var layer = layui.layer;
+		layer.open({
+			type:2,
+			content:"./loginWindow.html",
+			title:"用户登录",
+			area:["500px","250px"]
+		});
+	}); 
+};
+
+//显示权限不足窗口
+function displayPermissionWindow(){
+	layui.use('layer', function(){
+		var layer = layui.layer;
+		layer.open({
+			type:2,
+			title:0,
+			closeBtn:0,
+			content:"./permissionWindow.html",
+			area:["300px","400px"],
+			resize:0,
+		});
+	}); 
+};
+
+//权限检查
+function checkPermission(character){
+	if($.cookie("userID") == null){
+		displayLoginWindow();
+	}else if($.cookie("character")!=character){
+		displayPermissionWindow();
+	}else{
+		window.location.href = "./"+character+".html";
+	}
 }
