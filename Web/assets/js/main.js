989bef1ae18166182885f0f3a395094a8af5f3f9
yrc <haoran937@gmail.com>
2020-07-10 17:53:58 +0800

    修复 登录页面无法获取input的bug 添加 修改密码的功能 添加 第一次登录强制修改密码的支持

--- Web/assets/js/main.js
+++ Web/assets/js/main.js
@@ -58,55 +58,76 @@ function loadHeader(s){
 }
 
 //显示登录窗口
-$("body").append("<div id=\"loginWindow\" class=\"layui-hide\"></div>") 
+$("body").append("<div id='loginWindow' class='layui-hide'></div>") 
 $("#loginWindow").load("./loginWindow.html");
 function displayLoginWindow(){
 	layui.use('layer', function(){
+		var thisWindow = $("#loginWindow");
 		layer.open({
 			type:1,
 			title:0,
 			closeBtn:0,
 			resize:0,
 			skin:'login-popup',
-			content:$("#loginWindow").html(),
+			content:thisWindow,
 			area:["500px","399px"],
-			shadeClose:true,
+			shadeClose:1,
+			success:function(){
+				thisWindow.removeClass("layui-hide");
+			},
+			cancel: function(){ 
+				thisWindow.addClass("layui-hide");
+			}    
 		});
 	}); 
 };
 
 //显示修改密码窗口
-$("body").append("<div id=\"changePasswordWindow\" class=\"layui-hide\"></div>") 
+$("body").append("<div id='changePasswordWindow' class='layui-hide'></div>") 
 $("#changePasswordWindow").load("./changePasswordWindow.html");
-function displayChangePasswordWindow(){
+function displayChangePasswordWindow(flag = 1){
 	layui.use('layer', function(){
+		var thisWindow = $("#changePasswordWindow");
 		layer.open({
 			type:1,
 			title:0,
 			closeBtn:0,
 			resize:0,
 			skin:'login-popup',
-			content:$("#changePasswordWindow").html(),
+			content:thisWindow,
 			area:["500px","399px"],
-			shadeClose:true,
+			shadeClose:flag,
+			success:function(){
+				thisWindow.removeClass("layui-hide");
+			},
+			cancel: function(){ 
+				thisWindow.addClass("layui-hide");
+			}    
 		});
 	}); 
 };
 
 //显示权限不足窗口
-$("body").append("<div id=\"permissionWindow\" class=\"layui-hide\"></div>") 
+$("body").append("<div id='permissionWindow' class='layui-hide'></div>") 
 $("#permissionWindow").load("./permissionWindow.html");
 function displayPermissionWindow(){
 	layui.use('layer', function(){
+		var thisWindow = $("#permissionWindow");
 		layer.open({
 			type:1,
 			title:0,
 			closeBtn:0,
 			resize:0,
 			skin:"layer-ext-hide",
-			content:$("#permissionWindow").html(),
+			content:thisWindow,
 			area:["300px","400px"],
-			shadeClose:true,
+			shadeClose:0,
+			success:function(){
+				thisWindow.removeClass("layui-hide");
+			},
+			cancel: function(){ 
+				thisWindow.addClass("layui-hide");
+			}    
 		});
 	}); 
 };
@@ -120,6 +141,15 @@ function doCheck(data){
 }
 //权限检查
 function checkPermission(s){
+	if($.cookie("hasNotChangePassword")!=null){
+		$.removeCookie("userID");
+		$.removeCookie("username");
+		$.removeCookie("character");
+		$.removeCookie("hasNotChangePassword");
+	}
+	if(s=="index"){
+		return true;
+	}
 	if($.cookie("userID") == null){
 		displayLoginWindow();
 		return false;
@@ -142,7 +172,7 @@ function myHtml(){
 //登录表单
 layui.use('form', function(){
 	var form =layui.form;
-	form.on('submit(form-submit)', function(){
+	form.on('submit(login)', function(){
 		var username=$("#username").val();
 		var password=$("#password").val();
 		//md5加密
@@ -162,12 +192,46 @@ layui.use('form', function(){
 					layer.msg("用户名或密码错误");
 				}else if(data["loginCode"]==1){
 					layer.msg("登录成功");
-				}
+				}else if(data["loginCode"]==2){
+					$.cookie("hasNotChangePassword","ture");
+					displayChangePasswordWindow(0);
+				}	
 			},
 			error:function(){
 				layer.msg("服务器出错");
 			}
 
+		})
+		return false; //阻止表单跳转。如果需要表单跳转，去掉这段即可。
+	});
+});
+
+//修改密码
+layui.use('form', function(){
+	var form =layui.form;
+	form.on('submit(changePassword)', function(){
+		var newPassword=$("#newPassword").val();
+		var newPasswordTwins=$("#newPasswordTwins").val();
+		//md5加密
+		if(newPassword!=newPasswordTwins){
+			layer.msg("两次输入的密码不一致")
+			return false;
+		}
+		newPassword=$.md5(newPassword);
+		$.ajax({
+			type:"POST",
+			url:"/api/changePassword",
+			dataType:"json",
+			data:{
+				"newPassword":newPassword,
+			},
+			async:false,
+			success:function(){
+				$.removeCookie("hasNotChangePassword");
+			},
+			error:function(){
+				layer.msg("服务器出错");
+			}
 		})
 		return false; //阻止表单跳转。如果需要表单跳转，去掉这段即可。
 	});
--- Web/mockjs/mock.js
+++ Web/mockjs/mock.js
@@ -1,12 +1,12 @@
 Mock.mock("/api/login",function(){
 	var data = Mock.mock({
-		"loginCode|0-1":1,
+		"loginCode|2":1,
 	})
-	if(data["loginCode"]==1){
+	if(data["loginCode"]!=0){
 		$.cookie("userID","test");
 		$.cookie("username","学生A");
 		$.cookie("character","student");
-		location.href="./index.html";
+		if(data["loginCode"]==1)location.href="./"+$.cookie("character")+".html";
 	}
 	return JSON.stringify(data);
 });
@@ -22,6 +22,13 @@ Mock.mock("/api/examStart",function(){
 	var data = Mock.mock({
 		"examStart|0-1":1,
 	})
-	location.href="./exam.html";
+	if(data["examStart"]===1){
+		location.href="./exam.html";
+	}
 	return JSON.stringify(data);
+});
+
+Mock.mock("/api/changePassword",function(){
+	location.href="./"+$.cookie("character")+".html";
+	return true;
 });
