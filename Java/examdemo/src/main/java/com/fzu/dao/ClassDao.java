78b89c1a35e3d16be5d2665b9caab327dd08d29f
lvv <375042084@qq.com>
2020-08-22 19:23:13 +0800

    FileController(2.0)修改了student的上传 将导入数据库整合到了上传里

--- Java/examdemo/src/main/java/com/fzu/dao/ClassDao.java
+++ Java/examdemo/src/main/java/com/fzu/dao/ClassDao.java
@@ -15,10 +15,11 @@ public class ClassDao {
         for(Map.Entry<String, String> entry : map.entrySet()){
             String mapKey = entry.getKey();
             String mapValue = entry.getValue();
-            String sql="insert into exam_system.class_teacher(classroom, teacher_id) values (?,?)";//id自增
+            String sql="insert into exam_system.class_teacher(class_name, teacher_id) values (?,?)";//id自增
             Object[] objects=new Object[2];
             objects[0]=mapKey;
             objects[1]=mapValue;
+            System.out.println(mapKey);
             jdbcTemplate.update(sql,objects);
         }
 
