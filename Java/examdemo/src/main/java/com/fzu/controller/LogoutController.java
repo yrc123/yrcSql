8feaef916eb39495a547e68f30c7411dba5d8143
6586744 <67788880+6586744@users.noreply.github.com>
2020-08-22 21:19:34 +0800

    登录退出

---
+++ Java/examdemo/src/main/java/com/fzu/controller/LogoutController.java
@@ -0,0 +1,93 @@
+package com.fzu.controller;
+
+
+import com.alibaba.fastjson.JSONObject;
+import com.fzu.config.UUIDUtils;
+import com.fzu.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class LogoutController {
+    /*@Autowired
+    StudentService studentService=new StudentServiceImpl();
+    @Autowired
+    AdminService adminService=new AdminServiceImpl();
+    @Autowired
+    TeacherService teacherService=new TeacherServiceImpl();*/
+
+
+    @ResponseBody
+    @RequestMapping("/logout")
+    public Map<String, Object> logout(HttpServletResponse response, @RequestBody JSONObject jsonObject){
+        int status;
+        String username=(String)jsonObject.get("username");
+        String password=(String)jsonObject.get("password");
+        Map<String, Object> result = new HashMap<String, Object>();
+        System.out.println(username.substring(0,1));
+        //管理员
+        if (username.equals("admin")){
+            String userId = UUIDUtils.getUUID();
+            Cookie cookie1=new Cookie("userId",userId);
+            cookie1.setMaxAge(0);
+            cookie1.setPath("/");
+            Cookie cookie2=new Cookie("username",username);
+            cookie1.setMaxAge(0);
+            cookie2.setPath("/");
+            Cookie cookie3=new Cookie("character","admin");
+            cookie3.setMaxAge(0);
+            cookie3.setPath("/");
+            //status=adminService.admCheck(username, password);
+            //result.put("status", status);
+            response.addCookie(cookie1);
+            response.addCookie(cookie2);
+            response.addCookie(cookie3);
+        }
+        //教师
+        else if (username.substring(0,1).equals("T")){
+            String userId = UUIDUtils.getUUID();
+            Cookie cookie1=new Cookie("userId",userId);
+            Cookie cookie2=new Cookie("username",username);
+            Cookie cookie3=new Cookie("character","teacher");
+            cookie1.setMaxAge(0);
+            cookie2.setMaxAge(0);
+            cookie3.setMaxAge(0);
+            cookie1.setPath("/");
+            cookie2.setPath("/");
+            cookie3.setPath("/");
+            //status=teacherService.teaCheck(username,password);
+            //result.put("status", status);
+            response.addCookie(cookie1);
+            response.addCookie(cookie2);
+            response.addCookie(cookie3);
+        }
+        //学生
+        else{
+            String userId = UUIDUtils.getUUID();
+            Cookie cookie1=new Cookie("userId",userId);
+            Cookie cookie2=new Cookie("username",username);
+            Cookie cookie3=new Cookie("character","student");
+            cookie1.setMaxAge(0);
+            cookie2.setMaxAge(0);
+            cookie3.setMaxAge(0);
+            cookie1.setPath("/");
+            cookie2.setPath("/");
+            cookie3.setPath("/");
+            //status=studentService.stuCheck(username, password);
+            //result.put("status", status);
+            response.addCookie(cookie1);
+            response.addCookie(cookie2);
+            response.addCookie(cookie3);
+        }
+        return result;
+    }
+}
+
