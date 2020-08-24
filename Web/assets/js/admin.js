5d58bb6a1fb8cc73f1e450eb157619b2f4f863c1
Savona <905899183@qq.com>
2020-08-24 11:40:08 +0800

    lqw

--- Web/assets/js/admin.js
+++ Web/assets/js/admin.js
@@ -28,7 +28,7 @@ table.render({
 	},
 	toolbar:  '#teacherListBar' , //开启头部工具栏，并为其绑定左侧模板
     defaultToolbar: ['filter', 'exports', 'print'],
-	title:"班级考表",
+	title:"教师名单",
 	height: 'full-240',
 });
 
